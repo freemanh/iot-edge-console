efc1c7bc4809ceacaeecd6de5755fef432c12ad7
hanxi <han.xi@menpuji.com>
2020-09-09 15:00:55 +0800

    update menu name

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
             <el-menu-item index="1">概览</el-menu-item>
             <el-submenu index="2">
               <template slot="title">设备管理</template>
-              <el-menu-item index="2-1" route="/product">产品管理</el-menu-item>
-              <el-menu-item index="2-2" route="/device">设备管理</el-menu-item>
+              <el-menu-item index="2-1" route="/product">产品</el-menu-item>
+              <el-menu-item index="2-2" route="/device">设备</el-menu-item>
             </el-submenu>
             <el-menu-item index="3">规则引擎</el-menu-item>
           </el-menu>
